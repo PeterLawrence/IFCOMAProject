fcd8e242bfa43d475ececc5c23e77113b6e001d4
Peter J Lawrence <pjlawrence2000@googlemail.com>
2024-04-19 17:33:21 +0100

    updated config folders

--- ifc_config.py
+++ ifc_config.py
@@ -4,37 +4,23 @@ Created on Tue Dec 19 09:59:58 2023
 
 @author: P.J.Lawrence
 """
+import os
 
 # the default input file names if no command line parameters  are provided
 
-#FileName = "Office_Building-ArchitecturalModel.ifc"
-#Folder = "E:\\Support\BIM\\2011-09-14-Office-IFC\\Model-files\\"
-
-#FileName = "Clinic_Handover.ifc"
-#Folder = "E:\\Support\BIM\\2011-09-14-Clinic-IFC\\Model files\\"
-
-#Folder = "E:\\Support\\BIM\\Hotel\\v6\\"
-#FileName = "hotel-v6-4x3-level2-SBs-3xRequiredDoorFlowRate.ifc"
-
-#Folder = "E:\\Support\\BIM\\Hotel\\v7\\"
-#FileName = "hotel-v7-4x3-level2-SBs-DoorParams.ifc"
-
-Folder = "E:\\Support\\BIM\\Hotel\\"
+Folder = "TestCases"
 FileName = "hotel-v12.ifc"
 
-#Folder = "E:\\Support\\BIM\\TestModels\\"
-#FileName = "Hospital-Original-withIntDoors-InitialDesign.ifc" # No spaces defined
-#FileName = "StationNightclub-NoRoofVersion-final2.ifc" # No spaces defined
-#FileName = "StationNightclub-final2.ifc"
-
-IFCFile = Folder + FileName
+IFCFile = Folder + os.sep + FileName
 
 # the default output file names if no command line parameters  are provided
-EZFile = ".\\EZtest2.xml"
-MTAFile = ".\\IFCMeshLevels2.mta"
-GraphFile = "..\\BuildingGraph.html"
+OutputFolder = "Outputs"
+EZFile = OutputFolder + os.sep + "\\EZtest2.xml"
+MTAFile = OutputFolder + os.sep + "\\IFCMeshLevels2.mta"
+GraphFile = OutputFolder + os.sep + "\\BuildingGraph.html"
 
 # the default output file type if no command line parameters  are provided
+
 OutputType = "Graph"  # "DumpStairs" "Exodus" "EvacutionZ" "Graph"
 
 # show a basic plot of the model
